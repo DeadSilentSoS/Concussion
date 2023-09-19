fe8e33fb046a426a895e90dfc9415cce277d0abc
DeadSilentSoS <deadsilentsos@gmail.com>
2023-09-19 10:46:56 -0500

    fix

--- server/server.py
+++ server/server.py
@@ -10,7 +10,7 @@ PORT = 8080
 root = tk.Tk()
 root.title("Concussion")
 root.geometry("700x500")
-root.configure(bg="darkblue")
+root.configure(bg="#000017")
 
 # Create a socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -48,43 +48,43 @@ def handle_client(client_socket, client_address):
     update_status(f"Client at {client_address} disconnected.")
 
 # Create a frame for server title and status
-title_frame = tk.Frame(root, bg="darkblue")
+title_frame = tk.Frame(root, bg="#000046")
 title_frame.pack(fill=tk.BOTH, padx=10, pady=10)
 
 # Server title
-server_title = tk.Label(title_frame, text="Concussion", font=("Helvetica", 24, "bold"), fg="lightblue", bg="darkblue")
+server_title = tk.Label(title_frame, text="Concussion", font=("Helvetica", 24, "bold"), fg="#00c8ff", bg="#000046")
 server_title.pack()
 
 # Server status text
-server_status = tk.Label(root, text="Server Status: Not Running", font=("Helvetica", 16), fg="lightblue", bg="darkblue")
+server_status = tk.Label(root, text="Server Status: Not Running", font=("Helvetica", 16, "bold"), fg="#41E67B", bg="#000046")
 server_status.pack(padx=10, pady=10)
 
 # Frame for start and stop buttons
-button_frame = tk.Frame(root, bg="darkblue")
+button_frame = tk.Frame(root, bg="#000046")
 button_frame.pack(fill=tk.BOTH, padx=10, pady=10)
 
 # Start server button
-start_button = tk.Button(button_frame, text="Start Server", font=("Helvetica", 14), fg="darkblue", bg="lightblue")
+start_button = tk.Button(button_frame, text="Start Server", font=("Helvetica", 14, "bold"), fg="#0000A2", bg="#00FFEA")
 start_button.pack(side=tk.LEFT, padx=10, pady=10)
 
 # Stop server button
-stop_button = tk.Button(button_frame, text="Stop Server", font=("Helvetica", 14), fg="darkred", bg="#FF9999")
+stop_button = tk.Button(button_frame, text="Stop Server", font=("Helvetica", 14, "bold"), fg="#000000", bg="#FF0000")
 stop_button.pack(side=tk.LEFT, padx=10, pady=10)
 
 # Command input field and send button
-input_frame = tk.Frame(root, bg="darkblue")
+input_frame = tk.Frame(root, bg="#000046")
 input_frame.pack(fill=tk.BOTH, padx=10, pady=10)
 
 # Command input field
-command_entry = tk.Entry(input_frame, font=("Helvetica", 12), bg="white", fg="black")
+command_entry = tk.Entry(input_frame, font=("Helvetica", 12), bg="#000017", fg="#0296FF")
 command_entry.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10, pady=10)
 
 # Send command button
-send_button = tk.Button(input_frame, text="Send Command", font=("Helvetica", 12), fg="darkblue", bg="lightblue")
+send_button = tk.Button(input_frame, text="Send Command", font=("Helvetica", 12, "bold"), fg="#00002E", bg="#00C8FF")
 send_button.pack(side=tk.LEFT, padx=10, pady=10)
 
 # Text widget for display window
-text_widget = tk.Text(root, height=15, width=50, fg="black", bg="white")
+text_widget = tk.Text(root, height=15, width=50, fg="#0295FF", bg="#000046")
 text_widget.pack(fill=tk.BOTH, padx=10, pady=10)
 
 # Function to update the text widget with server status and messages
